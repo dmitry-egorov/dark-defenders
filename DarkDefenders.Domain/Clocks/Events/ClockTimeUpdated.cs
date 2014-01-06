830e2cc29e69e214f8d04fe9b22443d49e746679
Dmitry Egorov <degorov86@gmail.com>
2014-01-06 18:33:33 +0400

    missing file

---
+++ DarkDefenders.Domain/Clocks/Events/ClockTimeUpdated.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DarkDefenders.Domain.Events;
+using Infrastructure.DDDES.Implementations.Domain;
+using Infrastructure.Util;
+
+namespace DarkDefenders.Domain.Clocks.Events
+{
+    public class ClockTimeUpdated : EventBase<ClockId, ClockTimeUpdated>, IClockEvent
+    {
+        public TimeSpan NewTime { get; private set; }
+
+        public ClockTimeUpdated(ClockId clockId, TimeSpan newTime)
+            : base(clockId)
+        {
+            NewTime = newTime;
+        }
+
+        protected override string ToStringInternal()
+        {
+            return "World time updated: {0}, {1}".FormatWith(RootId, NewTime);
+        }
+
+        protected override bool EventEquals(ClockTimeUpdated other)
+        {
+            return NewTime.Equals(other.NewTime);
+        }
+
+        protected override int GetEventHashCode()
+        {
+            return NewTime.GetHashCode();
+        }
+
+        public void ApplyTo(IClockEventsReciever reciever)
+        {
+            reciever.Recieve(this);
+        }
+
+        public void Accept(IDomainEventReciever reciever)
+        {
+            reciever.Recieve(this);
+        }
+    }
+}
